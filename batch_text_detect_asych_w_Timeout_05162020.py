2ca4d647376f3551274e79391900f99948ac1788
Xiaohui Liu <xiaohuiliugis@gmail.com>
2020-05-17 17:16:45 -0400

    This is the final py to execute text detection

--- batch_text_detect_asych_w_Timeout_05162020.py
+++ batch_text_detect_asych_w_Timeout_05162020.py
@@ -5,11 +5,24 @@ set up:
     1) set os.environ to correct path to the json key file
     2) set the folder of input file: blobs = storage_client.list_blobs(bucket_name, prefix="001/3", delimiter="/")
     3) set the output folder path if needed
-If timeout error happens, a few places needs to be modified:
-	1) j (use this counter to mark the position of processed images. If one execution ended at j position, then next time start at j+1 position. This will determine the position from where images' uri will be sent to the array of request)  
+
+ **Loop through folders**
+Each run of the code is for N = 113,234 files in each of the 10 folders, which will take 1144 round( 1143*99 + 77(last round) = 113,234). In order to put the output file from the second folder(third...) into the same output_uri, count_2 needs to start from 1145,here is the full details on values of count_2 for each round:
+2(1145)
+3(2289)
+4(3433)
+5(4577)
+6(5721)
+7(6865)
+8(8009)
+9(9153)
+10(10297)
+
+***If timeout error happens, a few places needs to be modified:
+	1) j (use this counter to mark the position of processed images. If one execution ended at j position, then next time start at j+1 position. This will determine the position from where images' uri will be sent to the array of request). If no timeout error, j will start at 1, and comment out "if j>8910"
 	2) count_2(count_2 represent the # of rounds, each round process 99 images, add str(count_2) after output_uri can help identify each round, otherwise output file from new rounds could overwrite old round's output)
 If timeout happen, check how many rounds have been processed, i.e., count_2 = 91 in the variable list means round 90 has at least started, if count = 5, means 90*99 (past 90 rounds) + 5*99 (in the 91th round, processed 5 batches) = 9405(alreayd processed). Then set j =9406, count_2 = 91. 
-	3) Request =[], use console 
+	3) Request =[], 
 
 @author: Drs, Xiaohui Liu and Haipeng Tang
 """
@@ -33,19 +46,21 @@ storage_client = storage.Client()
 processing_size=99
 
 count=0
-#count_2=1
-count_2=91
+#count_2=1 #count_2=91
+count_2 = 1145
 # j is counter used to track # of processed images 
 j=0
    # Note: Client.list_blobs requires at least package version 1.17.0.
 try:
-    blobs = storage_client.list_blobs(bucket_name, prefix="001/3", delimiter="/")
+    blobs = storage_client.list_blobs(bucket_name, prefix="002/3", delimiter="/")
         
     for blob in blobs:
         j= j+1
         # for process w/ interruption, set j> # of processed imgs; for process without interruption,
         # set j >0, as it starts with 1, will always be greater than 0
-        if j>8910:
+       
+        if j>0:
+        #if j>8910:
         
             count=count+1        
             print ("Processed: %d images"%j)
@@ -86,7 +101,7 @@ try:
                 operation = client.async_batch_annotate_images(requests, output_config)
                 print("Waiting for operation to complete...")
                 # set the timeout =5000 will allow enough time to finish the processes
-                response = operation.result(5000)
+                response = operation.result(6000)
                    
                  # The output is written to GCS with the provided output_uri as prefix
                 gcs_output_uri = response.output_config.gcs_destination.uri
@@ -109,7 +124,7 @@ try:
     client = vision_v1.ImageAnnotatorClient()
     operation = client.async_batch_annotate_images(requests, output_config)
     print("Waiting for operation to complete...")
-    response = operation.result(5000)
+    response = operation.result(6000)
        
      # The output is written to GCS with the provided output_uri as prefix
     gcs_output_uri = response.output_config.gcs_destination.uri
